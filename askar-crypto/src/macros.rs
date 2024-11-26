ffc21018a665465d0d62de545728309a2b12324d
Andrew Whitehead <cywolf@gmail.com>
2024-11-26 14:10:46 -0800

    update Rust minimum version to 1.81; update dependencies
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- askar-crypto/src/macros.rs
+++
@@ -1,8 +0,0 @@
-macro_rules! maybeuninit_part {
-    ($val:expr, $member:tt) => {{
-        fn _to_mbi<T>(addr: *mut T) -> *mut ::core::mem::MaybeUninit<T> {
-            addr.cast()
-        }
-        unsafe { &mut *_to_mbi(::core::ptr::addr_of_mut!((*$val.as_mut_ptr()).$member)) }
-    }};
-}
